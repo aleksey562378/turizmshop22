2602ca12bed2426978fef479e54c5c9c2d085958
aleksey562378 <83020616ale@gmail.com>
2026-05-25 15:20:55 +0600

    fuck3

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://turizmshop-2.onrender.com/';
 
 // Create axios instance
 const api = axios.create({
